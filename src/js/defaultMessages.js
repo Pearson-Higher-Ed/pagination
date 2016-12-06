bd50bb686f055793cd79be6a165af9d6fd4d9d83
Rahm <urahmto@dnvcoml-67u3g8w.peroot.com>
2016-12-06 15:53:50 -0700

    Added i18n functionality

---
+++ src/js/defaultMessages.js
@@ -0,0 +1,16 @@
+import {defineMessages} from 'react-intl';
+
+export const messages = defineMessages({
+  activePage: {
+    id: 'activePage',
+    defaultMessage: 'Active page'
+  },
+  prevButton: {
+    id: 'prevButton',
+    defaultMessage: 'Previous'
+  },
+  nextButton: {
+    id: 'nextButton',
+    defaultMessage: 'Next'
+  }
+});
