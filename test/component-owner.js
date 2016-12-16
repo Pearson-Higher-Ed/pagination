bf2834e2d434d47fc50324d32814206a45d170a7
Nathan Kluth <nlkluth@gmail.com>
2016-12-16 09:59:21 -0700

    test

--- test/component-owner.js
+++ test/component-owner.js
@@ -1,64 +1,52 @@
-/* global describe it expect */
+/* global describe it beforeEach */
 
 import expect from 'expect';
 import expectJSX from 'expect-jsx';
 import React from 'react';
-import {IntlProvider} from 'react-intl';
 import TestUtils from 'react-addons-test-utils';
 import ComponentOwner from '../src/js/component-owner';
 
 expect.extend(expectJSX);
+const onSelect = () => true;
 
 describe('Component Owner Suite', () => {
   let renderer;
-  let intlProvider;
 
   beforeEach(() => {
     renderer = TestUtils.createRenderer();
-    intlProvider = new IntlProvider({locale: 'en'}, {});
   });
 
   it('shallowly renders the component owner using React TestUtils', () => {
-
-    const {intl} = intlProvider.getChildContext();
-
-    const targetData = {
-      elementId: 'test-target',
-      greeting: 'Hello world!'
-    };
-
     renderer.render(
       <ComponentOwner
-        data={targetData}
-        intl={intl}
         activePage={1}
+        items={3}
+        onSelect={onSelect}
+        maxButtons={5}
       />
-      , {intl}
     );
 
-
     const pagination = renderer.getRenderOutput();
-    console.log(pagination.props.children[0].props.children[1]);
+
+    expect(pagination.props.children.length).toEqual(3);
+    expect(pagination.props.children[1].length).toEqual(4);
+    expect(pagination.props.children[0].props.children[1].props.children).toEqual('Prev');
     expect(pagination.props.children[2].props.children[1].props.children).toEqual('Next');
   });
 
-  // it('renders the correct text when the button is clicked, in a document provided by jsdom', () => {
+  it('handles large buttons (showing ellipses)', () => {
 
-  //   const {intl} = intlProvider.getChildContext();
-  //   const targetData = {
-  //     elementId: 'test-target',
-  //     greeting: 'Hello test!'
-  //   };
-  //   const locale = 'en';
-  //   const translations = {
-  //     'en' : {}
-  //   };
+  })
 
-  //   const container = TestUtils.renderIntoDocument(<IntlProvider locale={locale} messages={translations[locale]}><ComponentOwner data={targetData} intl={intl} /></IntlProvider>);
-  //   const button = TestUtils.findRenderedDOMComponentWithTag(container, 'button');
-  //   const input =  TestUtils.findRenderedDOMComponentWithTag(container, 'input');
-  //   TestUtils.Simulate.click(button);
-  //   expect(input.value).toEqual('Hello test!');
-  // });
+  it('hanldes fewer pages than maxButtons', () => {
 
+  });
+
+  it('handles onSelect', () => {
+
+  });
+
+  it('handles defaults', () => {
+
+  });
 });
