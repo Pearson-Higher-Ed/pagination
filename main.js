23daddddd4bf85dbee718e26268e0c1e6d30c9ed
Brian Dunn <brian.dunn@pearson.com>
2017-09-05 17:26:22 -0600

    DES-544: rewriting existing unit tests with Enzyme

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Pagination from './src/js/component-owner';
 import PaginationButton from './src/js/PaginationButton';
-import PaginationIntl from './src/js/PaginationIntl';
 
 const onSelect = (eventKey) => {
   document.dispatchEvent(new CustomEvent('o-pagination-setActive', {
@@ -14,24 +13,16 @@ const onSelect = (eventKey) => {
 
 export default class PaginationContainer {
 
-  constructor(config) {
-    this.init(config);
-  }
-
-  init(config) {
-
-    const locale = config.locale ? config.locale : 'en';
-
+  constructor(props) {
     ReactDOM.render(
-      <PaginationIntl {...config} locale={locale} onSelect={onSelect} />,
-      document.getElementById(config.elementId)
+      <Pagination {...props} onSelect={onSelect} />,
+      document.getElementById(props.elementId)
     );
   }
-
 }
 //
 // For events, use the Origami naming convention of pre-pending with 'o.'
 //
 document.body.addEventListener('o.InitPagination', e => new PaginationContainer(e.detail));
 
-export { Pagination, PaginationButton, PaginationIntl };
+export { Pagination, PaginationButton };
